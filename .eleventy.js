ac22e508b9cb88b51fb5edd3df43f260f5c8383b
Ben Myers <ben.myers@okstate.edu>
2022-12-04 13:27:15 -0600

    Install WebC

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const webc = require('@11ty/eleventy-plugin-webc');
+
 /**
  * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
  * @typedef {ReturnType<import('@11ty/eleventy/src/defaultConfig')>} EleventyReturnValue
  * @type {(eleventyConfig: EleventyConfig) => EleventyReturnValue}
  */
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(webc);
+	
 	return {
 		dir: {
 			input: 'src'
